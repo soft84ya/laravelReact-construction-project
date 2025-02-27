786eeaa41e6f279cde2368b0a513735624579fa8
soft84ya <soft84ya@gmail.com>
2025-02-27 19:37:31 +0530

    v7 php life

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Home from './components/frontend/Home';
 import About from './components/frontend/About';
 import './assets/css/style.scss'
 import Services from './components/frontend/Services';
+import Projects from './components/frontend/Projects';
+import Blogs from './components/frontend/Blogs';
 
 
 function App() {
@@ -16,6 +18,8 @@ function App() {
           <Route path='/' element={<Home/>}/>
           <Route path='/about' element={<About/>}/>
           <Route path='/services' element={<Services/>}/>
+          <Route path='/projects' element={<Projects/>}/>
+          <Route path='/blogs' element={<Blogs/>}/>
         </Routes>
        </BrowserRouter>
     </>
